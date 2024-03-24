6cf8b462f54c06c2ee6aebd9a149666fb610e2b2
CarsonH19 <questline.carson@gmail.com>
2024-03-25 02:04:50 +0900

    fix: project.title

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-export default function Sidebar({ onAddProject, projects }) {
+export default function Sidebar({ onAddProject, projects, onOpenProject }) {
   return (
     <>
       <h3>YOUR PROJECTS</h3>
       <button onClick={onAddProject}>Add Projects</button>
       <ul>
         {projects &&
-          projects.map((project, index) => <li key={index}>{project.title}</li>)}
+          projects.map((project, index) => <li key={index} onClick={() => onOpenProject(index)} >{project.title}</li>)}
       </ul>
     </>
   );
