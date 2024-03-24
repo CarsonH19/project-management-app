1c03875022e4bb3b7ca0cfd807000e91e4d9a783
CarsonH19 <questline.carson@gmail.com>
2024-03-24 23:40:49 +0900

    feat: Sidebar and ProjectForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
+import { useState, useRef } from "react";
+import ProjectForm from "./components/ProjectForm";
+import Sidebar from "./components/Sidebar";
+import Tasks from "./components/Tasks";
+
 function App() {
+  const form = useRef();
+
+  const [projectForm, setProjectForm] = useState(false);
+  const [projects, setProjects] = useState([]);
+  const [projectTasks, setProjectTasks] = useState();
+
+  const handleAddProject = () => {
+    setProjectForm(true);
+  };
+
+  const handleSaveProject = (formData) => {
+    setProjects((prevProjects) => {
+      return [
+        ...prevProjects,
+        {
+          title: formData.title,
+          description: formData.description,
+          dueDate: formData.dueDate,
+        },
+      ];
+    });
+
+    console.log(projects);
+    setProjectForm(false);
+  };
+
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <Sidebar onAddProject={handleAddProject} projects={projects} />
+      {projectForm && (
+        <ProjectForm onSaveProject={handleSaveProject} ref={form} />
+      )}
+      {!projectForm && projectTasks && <Tasks />}
     </>
   );
 }
