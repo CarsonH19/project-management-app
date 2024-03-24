ccc91cb264a07649445a5d42fad71153da567e02
CarsonH19 <questline.carson@gmail.com>
2024-03-25 02:05:56 +0900

    feat: handlers

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import { useState, useRef } from "react";
 import ProjectForm from "./components/ProjectForm";
 import Sidebar from "./components/Sidebar";
-import Tasks from "./components/Tasks";
+import Project from "./components/Project";
 
 function App() {
+  // useRefs
   const form = useRef();
 
+  // useStates
   const [projectForm, setProjectForm] = useState(false);
-  const [projects, setProjects] = useState([]);
-  const [projectTasks, setProjectTasks] = useState();
+  const [projects, setProjects] = useState([
+    {
+      title: "Finish This Project",
+      description: "Complete the project management app for my React course.",
+      dueDate: "Mar 26, 2024",
+      tasks: [],
+    },
+  ]);
+  const [projectSelected, setProjectSelected] = useState(null);
 
-  const handleAddProject = () => {
+  // Handlers
+  const handleProjectForm = () => {
     setProjectForm(true);
   };
 
@@ -19,24 +29,63 @@ function App() {
       return [
         ...prevProjects,
         {
-          title: formData.title,
-          description: formData.description,
-          dueDate: formData.dueDate,
+          title: formData.title || "Nothing Important",
+          description: formData.description || "blah blah blah........",
+          dueDate: formData.dueDate || "2024-03-25",
+          tasks: [],
         },
       ];
     });
-
-    console.log(projects);
     setProjectForm(false);
   };
 
+  const handleCancel = () => {
+    setProjectForm(false);
+  };
+
+  const handleProjectSelect = (index) => {
+    setProjectSelected(index);
+  };
+
+  const handleDelete = (projectIndex) => {
+    setProjects((prevProjects,) => {
+      // Filter out the project to be deleted
+      return prevProjects.filter((_, index) => index !== projectIndex);
+    });
+  };
+
+  console.log(projects);
+  console.log(projectSelected);
+  console.log(projectForm);
+
   return (
     <>
-      <Sidebar onAddProject={handleAddProject} projects={projects} />
+      <Sidebar
+        onAddProject={handleProjectForm}
+        projects={projects}
+        onOpenProject={handleProjectSelect}
+      />
+
       {projectForm && (
-        <ProjectForm onSaveProject={handleSaveProject} ref={form} />
+        <ProjectForm
+          onClear={handleCancel}
+          onSave={handleSaveProject}
+          ref={form}
+        />
       )}
-      {!projectForm && projectTasks && <Tasks />}
+
+      {!projectForm && projectSelected && (
+        <Project onDeleteProject={handleDeleteProject} projects={projects} projectSelected={projectSelected} />
+      )}
+
+      {!projectForm && !projectSelected && (
+        <div>
+          <img src="src/assets/no-projects.png" alt="Clipboard and Pen Image" />
+          <h2>No Project Selected</h2>
+          <p>Select a project or get started with a new one</p>
+          <button>Create New Project</button>
+        </div>
+      )}
     </>
   );
 }
