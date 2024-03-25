15ab1717cbb67bc2ae71f9fd0267fa9c1412d68d
CarsonH19 <questline.carson@gmail.com>
2024-03-26 00:13:45 +0900

    feat: Task & NewTask state/handlers

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,25 @@ function App() {
     projects: [],
   });
 
+  function handleAddTask(text) {
+    setProjectsState((prevState) => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId,
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        tasks: [newTask, ...prevState.tasks],
+      };
+    });
+  }
+
+  function handleDeleteTask() {}
+
   function handleSelectProject(id) {
     setProjectsState((prevState) => {
       return {
@@ -72,7 +91,13 @@ function App() {
   );
 
   let content = (
-    <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+    <SelectedProject
+      project={selectedProject}
+      onDelete={handleDeleteProject}
+      onAddTask={handleAddTask}
+      onDeleteTask={handleDeleteTask}
+      tasks={projectsState.tasks}
+    />
   );
 
   if (projectsState.selectedProjectId === null) {
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,10 +1,25 @@
-export default function Tasks() {
+import NewTask from "./NewTask";
+
+export default function Tasks({ tasks, onAdd, onDelete }) {
   return (
     <section>
       <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
-      NEW TASK
-      <p className="text-stone-800 mb-4">This project doesn't have any tasks yet.</p>
-      <ul></ul>
+      <NewTask onAdd={onAdd} />
+      {tasks.length === 0 && (
+        <p className="text-stone-800 my-4">
+          This project doesn't have any tasks yet.
+        </p>
+      )}
+      {tasks.length > 0 && (
+        <ul className="p-4 mt-8 rounded-md bg-stone-100">
+          {tasks.map((tasks) => {
+            <li key={tasks.id} className="flex justify-between my-4">
+              <span>{tasks.text}</span>
+              <button className="text-stone-700 hover:text-red-500">Clear</button>
+            </li>;
+          })}
+        </ul>
+      )}
     </section>
-  )
+  );
 }
