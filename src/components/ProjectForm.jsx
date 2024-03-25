2fe77aba638dd276bb5b91c6f19111fa405a10a9
CarsonH19 <questline.carson@gmail.com>
2024-03-25 21:30:19 +0900

    style: ProjectForm

--- src/components/ProjectForm.jsx
+++ src/components/ProjectForm.jsx
@@ -1,4 +1,5 @@
 import { useState, forwardRef } from "react";
+import Input from "./Input";
 
 const ProjectForm = forwardRef(function ProjectForm({ onCancel, onSave }, ref) {
   const [formData, setFormData] = useState({
@@ -27,40 +28,55 @@ const ProjectForm = forwardRef(function ProjectForm({ onCancel, onSave }, ref) {
   };
 
   return (
-    <>
-      <form ref={ref} onSubmit={handleSubmit}>
-        <div>
-          <button onClick={onCancel}>Cancel</button>
-          <button type="submit">Save</button>
-        </div>
-        <div>
-          <label htmlFor="title">Title</label>
-          <input
-            value={formData.title}
-            type="text"
-            name="title"
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="description">Description</label>
-          <textarea
-            value={formData.description}
-            name="description"
-            onChange={handleChange}
-          ></textarea>
-        </div>
-        <div>
-          <label htmlFor="dueDate">Due Date</label>
-          <input
-            value={formData.dueData}
-            type="date"
-            name="dueDate"
-            onChange={handleChange}
-          />
-        </div>
-      </form>
-    </>
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <li>
+          <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+        </li>
+        <li>
+          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
+        </li>
+      </menu>
+      <div>
+        <Input label="Title" />
+        <Input label="Description" textarea />
+        <Input label="Due Date" />
+      </div>
+    </div>
+    // <>
+    //   <form ref={ref} onSubmit={handleSubmit}>
+    //     <div>
+    //       <button onClick={onCancel}>Cancel</button>
+    //       <button type="submit">Save</button>
+    //     </div>
+    //     <div>
+    //       <label htmlFor="title">Title</label>
+    //       <input
+    //         value={formData.title}
+    //         type="text"
+    //         name="title"
+    //         onChange={handleChange}
+    //       />
+    //     </div>
+    //     <div>
+    //       <label htmlFor="description">Description</label>
+    //       <textarea
+    //         value={formData.description}
+    //         name="description"
+    //         onChange={handleChange}
+    //       ></textarea>
+    //     </div>
+    //     <div>
+    //       <label htmlFor="dueDate">Due Date</label>
+    //       <input
+    //         value={formData.dueData}
+    //         type="date"
+    //         name="dueDate"
+    //         onChange={handleChange}
+    //       />
+    //     </div>
+    //   </form>
+    // </>
   );
 });
 
